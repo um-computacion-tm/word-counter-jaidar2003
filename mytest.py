3642422d1073b13efc718123e9a2d265d0b3bba2
Juanma Aidar <vboxuser@linux.myguest.virtualbox.org>
2023-04-10 22:52:51 -0300

    arreglo de unittest

--- mytest.py
+++ mytest.py
@@ -1,15 +1,14 @@
 import unittest
-from contador import contar_palabras
+from contador1 import contar_palabras
 class TestContarPalabras(unittest.TestCase):
     
     def test_contar_palabras(self):
-        frase = "Hola mundo, hola Python, holA america, alemania "
+        frase = "Hola mundo hola Python "
         resultado = contar_palabras(frase)
-        self.assertEqual(resultado["Hola"], 3)
+        self.assertEqual(resultado["Hola"], 1)
         self.assertEqual(resultado["mundo"], 1)
-        self.assertEqual(resultado["hola,"], 1)
-        self.assertEqual(resultado["Phyton"], 1)
-        self.assertEqual(resultado["holA"], 1)
-        
+        self.assertEqual(resultado["hola"], 1)
+        self.assertEqual(resultado["Python"], 1)
+           
 if __name__ == '_main_':
     unittest.main()
